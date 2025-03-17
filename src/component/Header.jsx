c49d966e642ab738415a2345917997c2ab45983c
Anil khand <anilkhandthakuri@gmail.com>
2025-03-17 23:00:18 +1100

    Done with updates

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,22 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <>
-     <div>
-      <nav className="navbar navbar-light bg-light justify-content-around">
-        <h1 className="navbar-brand bg-light"style={{border: "none", margin: "1rem"}}>Finance Tracker</h1>
-
-        <header class="flex justify-around items-center py-6">
-            
-            <nav>
-                <a href="/" class="text-gray-400 mx-2">Dashboard</a>
-                <a href="/transaction" class="text-gray-400 mx-2">Transaction</a>
-                <a href="#" class="text-gray-400 mx-2">Logout</a>
-            </nav>
-        </header>
-        
-      </nav>
-    </div>
-    
+   
 
     </>
   )
