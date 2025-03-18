994edcf42916722a5ae8b3e4132f401f181aa641
Anil khand <anilkhandthakuri@gmail.com>
2025-03-18 21:44:23 +1100

    Done with updates

--- src/helpers/axiosHelper.js
+++ src/helpers/axiosHelper.js
@@ -2,18 +2,6 @@ import axios from "axios";
 
 export const apiProcessor = async (axiosObj) => {
   try {
-    const { url, method, data, headers } = axiosObj;
-    const response = await axios({
-      url,
-      method,
-      data,
-      headers,
-    });
-    return response.data;
-  } catch (error) {
-    return {
-      status: "error",
-      message: error?.response?.data?.message || error.message,
-    };
+   
   }
 };
