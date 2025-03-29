1299b77fa7f637928d3ba143d1b7c7f14f02c8bc
Anil khand <anilkhandthakuri@gmail.com>
2025-03-29 22:49:10 +1100

    Done with updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Signup from "./pages/Signup.jsx";
+import Signup from "../pages/Signup.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Signin from "./pages/Signin.jsx";
 
